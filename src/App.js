e0bb56f49a00a5bcb4a8c004372454fd3c79118e
hwavy <haruki4dev@gmail.com>
2021-10-05 15:41:22 +0900

    犬画像を表示

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">React App</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <div className="columns is-vcentered is-multiline">
+            <div className="column is-3">
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image">
+                    <img
+                      src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+                      alt="cute dog"
+                    />
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        {/* <p>Dog images are retrieved from Dog API</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+        </p> */}
+      </div>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
