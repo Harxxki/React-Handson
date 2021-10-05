68173a9e57e4efda1c90655f8c1994b890550f7e
hwavy <haruki4dev@gmail.com>
2021-10-05 18:26:16 +0900

    ランダムにスターウォーズのキャラクターを取得

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import React from "react";
 
 function Header() {
   return (
-    <header className="hero is-dark is-bold">
+    <header className="hero">
       <div className="hero-body">
         <div className="container">
           <h1 className="title">React App</h1>
@@ -13,38 +14,49 @@ function Header() {
 }
 
 function Main() {
+  const [foo, setData] = React.useState(
+    "Press the button to get the Star Wars characters!"
+  );
+  const handleClick = () => {
+    fetchData(
+      "https://swapi.dev/api/people/" + Math.floor(Math.random() * 11)
+    ).then((data) => {
+      console.log(data);
+      setData(data);
+    });
+  };
   return (
     <main>
-      <section className="section">
-        <div className="container">
-          <div className="columns is-vcentered is-multiline">
-            <div className="column is-3">
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image">
-                    <img
-                      src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                      alt="cute dog"
-                    />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="columns is-centered is-vcentered">
+        <div class="column　is-vcentered">
+          <button class="button is-primary" onClick={handleClick}>
+            Get
+          </button>
         </div>
-      </section>
+      </div>
+      <div class="columns is-centered">
+        <div class="column has-text-centered">
+          <p>{foo}</p>
+        </div>
+      </div>
     </main>
   );
 }
 
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.name;
+}
+
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer" id="footer">
       <div className="content has-text-centered">
-        {/* <p>Dog images are retrieved from Dog API</p>
+        <p>&copy; 2021 Haruki Mori</p>
         <p>
-          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
-        </p> */}
+          <a href="https://github.com/hwavy">GitHub</a>
+        </p>
       </div>
     </footer>
   );
